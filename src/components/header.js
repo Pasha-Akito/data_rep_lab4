03ac98c98662f8974f9079c6d7eaa953ce278819
Pasha-Akito <1masterpasha@gmail.com>
2020-10-18 19:47:08 +0100

    Lab3 upload

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export class Header extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>This is from the header Component!!!</h1>
+            </div>
+        );
+    }
+}
+
